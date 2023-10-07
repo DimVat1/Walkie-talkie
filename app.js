2580afc6ee5d6a6d7cd2a39dae0ab7de8b38c7eb
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-10-07 18:50:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ document.addEventListener("DOMContentLoaded", function () {
     let mediaRecorder;
     let audioChunks = [];
 
+    let peerConnection;
+    let dataChannel;
+
     createRoomButton.addEventListener("click", createRoom);
     joinRoomButton.addEventListener("click", joinRoom);
     recordButton.addEventListener("click", toggleRecording);
@@ -58,27 +61,50 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function startRecording() {
-        // Add recording logic as previously shown
-        // ...
+        navigator.mediaDevices
+            .getUserMedia({ audio: true })
+            .then(function (stream) {
+                mediaRecorder = new MediaRecorder(stream);
+
+                mediaRecorder.ondataavailable = function (event) {
+                    if (event.data.size > 0) {
+                        audioChunks.push(event.data);
+                    }
+                };
+
+                mediaRecorder.onstop = function () {
+                    const audioBlob = new Blob(audioChunks, { type: "audio/wav" });
+                    displayAudioMessage(audioBlob);
+                    audioChunks = [];
+                    sendAudioData(audioBlob); // Send the audio recording
+                };
+
+                mediaRecorder.start();
+                isRecording = true;
+                recordButton.textContent = "Stop Recording";
+                sendButton.disabled = true; // Disable "Send" while recording
+            })
+            .catch(function (error) {
+                console.error("Error accessing microphone: " + error);
+            });
     }
 
     function stopRecording() {
-        // Add stop recording logic as previously shown
-        // ...
-    }
-
-    function sendAudioData(audioBlob) {
-        // Send audio data logic as previously shown
-        // ...
+        if (mediaRecorder && mediaRecorder.state === "recording") {
+            mediaRecorder.stop();
+            isRecording = false;
+            recordButton.textContent = "Record";
+            sendButton.disabled = false; // Re-enable "Send" after stopping
+        }
     }
 
     function sendMessage() {
         const messageText = messageInput.value.trim();
 
         if (messageText !== "") {
-            const messageWithEmoji = `${messageText} 🎙️`;
-            displayMessage(messageWithEmoji);
+            displayMessage(messageText);
             messageInput.value = ""; // Clear the input field
+            sendTextMessage(messageText); // Send the text message
         }
     }
 
@@ -89,5 +115,50 @@ document.addEventListener("DOMContentLoaded", function () {
         chatBox.appendChild(messageElement);
     }
 
-    // Other functions and WebRTC-related code as previously shown
+    function displayAudioMessage(audioBlob) {
+        const audioElement = document.createElement("audio");
+        audioElement.src = URL.createObjectURL(audioBlob);
+        audioElement.controls = true;
+
+        const messageElement = document.createElement("div");
+        messageElement.classList.add("message");
+        messageElement.appendChild(audioElement);
+        chatBox.appendChild(messageElement);
+    }
+
+    // Add other WebRTC-related functions and logic here
+    // Replace these placeholder functions with actual implementations
+    function sendIceCandidate(candidate) {
+        // Implement the logic to send the ICE candidate to the other peer
+    }
+
+    function handleIncomingOffer(offer) {
+        // Implement the logic to handle an incoming offer
+    }
+
+    function handleIncomingAnswer(answer) {
+        // Implement the logic to handle an incoming answer
+    }
+
+    function sendAudioData(audioBlob) {
+        // Implement the logic to send audio data to the other peer
+        // You'll need to use the dataChannel to send audioBlob to the peer
+    }
+
+    function sendTextMessage(text) {
+        // Implement the logic to send a text message to the other peer
+        // You'll need to use the dataChannel to send text messages to the peer
+    }
+
+    function createPeerConnection() {
+        // Implement the logic to create a WebRTC peer connection
+        // Configure ICE servers, create data channel, and handle other WebRTC setup
+    }
+
+    // Handle incoming ICE candidates from the other peer
+    function handleIncomingIceCandidate(candidate) {
+        // Implement the logic to handle incoming ICE candidates
+    }
+
+    // Implement the rest of your WebRTC-related code here
 });
